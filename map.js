ee455a7d50665b8058c93fc90a7c4aafe8ca170a
Luke254 <lukaszg257@gmail.com>
2021-02-08 00:37:05 +0100

    load from json

--- map.js
+++ map.js
@@ -1,3 +1,5 @@
+var locations;
+
 var map = L.map('map', {
     crs: L.CRS.Simple,
     minZoom: -3
@@ -7,32 +9,30 @@ var bounds = [[0,0], [1986,3820]];
 var image = L.imageOverlay('maps/customs.png', bounds).addTo(map);
 map.fitBounds(bounds);
 
-
-// forteca
-var polygon = L.polygon([[634, 1959],[653, 2066],[433, 2105],[411, 2000]], {color: 'blue'}).addTo(map);
-var marker = new L.marker([543, 2032.5], { opacity: 0.01 });
-marker.bindTooltip("Forteca", {
-    permanent: true, 
-    className: "my-label", 
-    offset: [0, 0], 
-    direction: 'bottom',
-    sticky: true });
-marker.addTo(map);
-
-// czerwony
-var polygon2 = L.polygon([[684, 492],[679, 611],[455, 604],[460, 485]], {color: 'red'}).addTo(map);
-var marker2 = new L.marker([581, 545], { opacity: 0.01 });
-marker2.bindTooltip("Czerwony", {
-    permanent: true, 
-    className: "my-label", 
-    offset: [0, 0], 
-    direction: 'bottom',
-    sticky: true });
-marker2.addTo(map);
-
 map.on('click', function(e){
     var coord = e.latlng;
     var lat = coord.lat;
     var lng = coord.lng;
     console.log("y: " + lat + " x: " + lng);
+});
+
+fetch('./locations/customs.json')
+.then(response => response.json())
+.then(json => {
+    locations = json;
+    for (const location of locations) {
+        if (location.radius > 0) {
+            L.circle(location.point, {radius: location.radius, color: location.color}).addTo(map);
+        } else {
+            L.polygon(location.corners, {color: location.color}).addTo(map);
+        }
+        let marker = new L.marker(location.point, { opacity: 0.01 });
+        marker.bindTooltip(location.name, {
+            permanent: true, 
+            className: "my-label", 
+            offset: [0, 0], 
+            direction: 'top',
+            sticky: true });
+        marker.addTo(map);
+    }
 });
